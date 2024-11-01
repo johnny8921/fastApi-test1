cc371f80ee903e2b8cb88e463491d73d09dbc7ba
Evgeniy Pushkarev <39298169+johnny8921@users.noreply.github.com>
2024-11-01 17:16:40 +0300

    finish lesson 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -15,43 +16,65 @@ async def contacts() -> int:
     return 34
 
 
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+class Post(BaseModel):
+    id: int
+    title: str
+    body: str
+    author: User
+
+
+users = [
+    {'id': 1, 'name': 'John Doe', 'age': 30},
+    {'id': 2, 'name': 'Диана', 'age': 25},
+    {'id': 3, 'name': 'Bob Smith', 'age': 40},
+]
+
 posts = [
     {
         "id": 1,
         "title": "title of post 1",
         "body": "body of post 1",
+        'author': users[1]
 
     },
     {
         "id": 2,
         "title": "title of post 2",
         "body": "body of post 2",
+        'author': users[2]
 
     },
     {
         "id": 3,
         "title": "title of post 3",
         "body": "body of post 3",
+        'author': users[0]
 
     }
 ]
 
 
 @app.get('/items')
-async def items() -> list[dict]:
-    return posts
+async def items() -> List[Post]:
+    return [Post(**post) for post in posts]
 
 
 @app.get('/items/{id}')
-async def item(id: int) -> dict:
+async def item(id: int) -> Post:
     for post in posts:
         if post['id'] == id:
-            return post
+            return Post(**post)
     raise HTTPException(status_code=404, detail="Post not found")
 
 
 @app.get('/search')
-async def search(post_id: Optional[int]) -> dict:
+async def search(post_id: Optional[int]) -> Post | Dict[str, Optional[Post]]:
     if post_id:
         return await item(post_id)
-    return {"message": "Post not found"}
+    return {"data": None}
